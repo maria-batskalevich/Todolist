ff32b0fb660d5611fde746b6f6ebd5c12c5a7a79
maria-batskalevich <maria.batskalevich@gmail.com>
2021-10-19 17:46:46 +0300

    Updates

---
+++ precache-manifest.0f8814e4caf58e3f8e9e69edae0a0b00.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f3281fe6257c335b72dcf476c6a1cc43",
+    "url": "/todolist/index.html"
+  },
+  {
+    "revision": "70e4a6bd64b3095790ea",
+    "url": "/todolist/static/css/main.84802909.chunk.css"
+  },
+  {
+    "revision": "2bb74ffe3412fbe07b36",
+    "url": "/todolist/static/js/2.bf29800e.chunk.js"
+  },
+  {
+    "revision": "89a1b2dcd30c03705b2bceeb141b76b6",
+    "url": "/todolist/static/js/2.bf29800e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "70e4a6bd64b3095790ea",
+    "url": "/todolist/static/js/main.0e73d959.chunk.js"
+  },
+  {
+    "revision": "471e54e937a3aef18e16",
+    "url": "/todolist/static/js/runtime-main.283d8678.js"
+  }
+]);
